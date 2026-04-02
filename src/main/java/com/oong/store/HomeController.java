6289ba907ae431de00c45f08c8ce54aeb672cf2b
oong <mkhalil26122000@gmail.com>
2026-04-03 02:23:33 +0700

    Customize application properties

--- src/main/java/com/oong/store/HomeController.java
+++ src/main/java/com/oong/store/HomeController.java
@@ -1,12 +1,18 @@
 package com.oong.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+
+    @Value("${spring.application.name}")
+    private String appName;
+
     @RequestMapping("/")
     public String index() {
+        System.out.println(appName);
         return "index.html";
     }
 }
