8a20bf7f84460ca1b63a804010a19d280def17dc
oong <mkhalil26122000@gmail.com>
2026-04-03 01:56:05 +0700

    Built first spring controller

---
+++ src/main/java/com/oong/store/HomeController.java
@@ -0,0 +1,12 @@
+package com.oong.store;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
+}
